6f2f647ccf620b2f639532fd94943e674b3dfb8c
= <=>
2024-07-04 21:36:53 +0100

    adding files

---
+++ modern_interface.py
@@ -0,0 +1,33 @@
+# Install customtkinter
+
+import customtkinter
+
+customkinter.set_appearance_mode("dark")  #you can also change this "System"
+customkinter.set_defualt_color_theme("green")
+
+root = customtkinter.CTk()
+root.geometry("500x350")
+
+def login():
+    print("Test")
+    
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=50, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login System", text_font=("Roboto", 24))
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButon(master=frmae, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
+                        
